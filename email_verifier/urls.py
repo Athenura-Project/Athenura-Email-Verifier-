6567ed1dacea15233845f7d001e8cc93967bcf45
Jeetendra Dhangar <jeetendradhangar6@gmail.com>
2026-02-17 02:39:30 +0530

    UI working

--- email_verifier/urls.py
+++ email_verifier/urls.py
@@ -1,11 +1,3 @@
-# from django.urls import path
-# from . import views 
-# app_name = 'email_verifier'
-# urlpatterns = [
-#     path('', views.index, name='index'),   
-#     path('', views.download_verified_emails, name="download_csv"),
-#     path('', views.download_verified_emails_xlsx, name="download_xlsx"),
-# ]
 
 from django.urls import path
 from . import views
@@ -13,7 +5,7 @@ from . import views
 app_name = "email_verifier"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("email-verifier/", views.index, name="index"),
 
     path(
         "download/csv/",
