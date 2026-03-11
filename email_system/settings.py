bcac532239bce58cbdc46a2f1a072f4c62338fa1
Sameersingh001 <info.athenura@gmail.com>
2026-03-11 13:51:34 +0530

    Commit

--- email_system/settings.py
+++ email_system/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-i7xea#kfuoja4p@$g+k*p^w-i2*(q6j!h%9(5)m7e)w2mb$m-6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
